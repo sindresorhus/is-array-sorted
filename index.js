cf663a68746e134a4f03159f69177a2e7133fb79
Federico Brigante <bfred-it@users.noreply.github.com>
2017-01-23 22:56:48 +0900

    Default parameters; avoid setting vars and conditionals

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 'use strict';
-module.exports = (arr, comp) => {
+module.exports = (arr, comp = (a, b) => Number(a > b)) => {
 	for (let i = 0; i < arr.length - 1; i++) {
-		const a = arr[i];
-		const b = arr[i + 1];
-
-		if (comp ? comp(a, b) > 0 : a > b) {
+		if (comp(arr[i], arr[i + 1]) > 0) {
 			return false;
 		}
 	}
