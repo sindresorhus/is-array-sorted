82dd84bd897210156b189fd279650976e1448079
Max Graey <maxgraey@gmail.com>
2017-01-24 18:39:03 +0200

    add array check

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const defaultComparator = (a, b) => a > b;
 module.exports = (arr, comp) => {
+	if (!Array.isArray(arr)) {
+		return false;
+	}
 	comp = comp || defaultComparator;
 	var a = arr[0];
 	for (var i = 1, len = arr.length; i < len; i++) {
